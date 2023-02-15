205f83ae4a5c5d2b46986730c6bd88a171e3668f
PratikKothari30 <119115527+PratikKothari30@users.noreply.github.com>
2023-02-15 11:01:41 +0530

    Add files via upload

---
+++ listexample.py
@@ -0,0 +1,64 @@
+mylist = ["prashant","Ashish","komal","ankush","Ashish",77,"sandip",60.52,"prashant"]
+print(type(mylist))
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[-1])
+print(mylist[2:5])
+print(mylist[:5])
+print(mylist[1:])
+print(mylist[::-1])
+print(mylist[1:8:2])
+
+'''mylist[2]="pratik"
+print(mylist)'''
+
+'''print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+print(mylist[4])
+print(mylist[5])'''
+
+'''for i in mylist:
+    print(i)'''
+    
+''' i=0 prashant
+    i=1 Ashish
+    i=2 Rahul
+    i=3 Sandip
+:
+:
+'''
+'''mylist.append('harsh')
+mylist.append("laxman")
+print(mylist)'''
+
+'''mylist.insert(3,"sanket")
+print(mylist)'''
+        
+'''mylist.remove("sandip")
+print(mylist)'''
+
+'''newlist = mylist.copy()
+print(newlist)'''
+
+mytuple=("prashant","Ashish","Rahul","sandip","komal","ankush","rajesh",23,3.15,77,"sandip")
+print(mytuple)
+print(type(mytuple))
+mytuple[2]="sunil"
+print(mytuple)
+
+mylist = [['prashant','jha'],['88.56'],[44002,"yyy"]]
+print("example of multidimensional list : ")
+print(mylist)
+print(mylist[0][0])
+print(mylist[0][1])
+print(mylist[1])
+print(mylist[2][0])
+print(mylist[2][1])
+#instead of multidimensional list dictionary will be better
+
+mylist=[44,22,77,0,9,88]
+print(0 in mylist)
+print(100 in mylist)
