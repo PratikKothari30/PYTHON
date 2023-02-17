abb291209340cbd976544da40ede9ec8b9b93a45
Pratik Kothari <119115527+PratikKothari30@users.noreply.github.com>
2023-02-17 12:10:54 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,28 @@
+#Function_1
+def Hello():
+    print("Hello World")
+
+#Function_2
+def welcome(Fname,Lname):
+    print("First name :",Fname)
+    print("Last name :",Lname)
+    
+#Function_3
+def cube(num):
+    print(f"Cube of {num} is :",num*num*num)
+    
+    
+ #Square of number
+def square(n):
+    print(n*n)
+    
+#variable
+pi = 3.14233467
+
+#Function_4
+def login(Username,Password):
+        
+        if Username == Password:
+            print("Login Sucessfully")
+        else:
+            print("login Failed")
